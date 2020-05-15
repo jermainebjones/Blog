f52a42f656f8794111d0808384395fd7f8104a0b
jermainebjones <39226126+jermainebjones@users.noreply.github.com>
2020-05-15 00:02:46 -0400

    Initial commit

---
+++ app/src/main/java/com/jermainebjonesgmail/blog/Activities/AddPostActivity.java
@@ -0,0 +1,155 @@
+package com.jermainebjonesgmail.blog.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.jermainebjonesgmail.blog.Model.Blog;
+import com.jermainebjonesgmail.blog.R;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddPostActivity extends AppCompatActivity {
+    private ImageButton mPostImage;
+    private EditText mPostTitle;
+    private EditText mPostDesc;
+    private Button mSubmitButton;
+    private StorageReference mStorage;
+    private DatabaseReference mPostDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private ProgressDialog mProgress;
+    private Uri mImageUri;
+    private static final int GALLERY_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_post);
+
+        mProgress = new ProgressDialog(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        mStorage = FirebaseStorage.getInstance().getReference();
+
+        mPostDatabase = FirebaseDatabase.getInstance().getReference().child("MBlog");
+
+        mPostImage = (ImageButton) findViewById(R.id.imageButton);
+        mPostTitle = (EditText) findViewById(R.id.postTitleEt);
+        mPostDesc = (EditText) findViewById(R.id.descriptionEt);
+        mSubmitButton = (Button) findViewById(R.id.submitPost);
+
+        mPostImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GALLERY_CODE);
+            }
+        });
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Posting to our database.
+                startPosting();
+
+
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_CODE && resultCode == RESULT_OK) {
+            mImageUri = data.getData();
+            mPostImage.setImageURI(mImageUri);
+
+        }
+    }
+
+    private void startPosting() {
+
+        mProgress.setMessage("Posting to blog...");
+        mProgress.show();
+
+        final String titleVal = mPostTitle.getText().toString().trim();
+        final String descVal = mPostDesc.getText().toString().trim();
+
+        if (!TextUtils.isEmpty(titleVal) && !TextUtils.isEmpty(descVal)
+                && mImageUri != null) {
+
+            // start the uploading...
+            // mImageUri.getLastPathSegment() == /image/myphoto.jpeg
+
+            final StorageReference filepath = mStorage.child("MBlog_images").child(mImageUri.getLastPathSegment());
+            filepath.putFile(mImageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        filepath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                String downloadUrl = uri.toString();
+
+                                DatabaseReference newPost = mPostDatabase.push();
+
+                                Map<String, String> dataToSave = new HashMap<>();
+                                dataToSave.put("title", titleVal);
+                                dataToSave.put("desc", descVal);
+                                dataToSave.put("image", downloadUrl.toString());
+                                dataToSave.put("timestamp", String.valueOf(java.lang.System.currentTimeMillis()));
+                                dataToSave.put("userid", mUser.getUid());
+
+                                newPost.setValue(dataToSave);
+
+                                mProgress.dismiss();
+
+                                startActivity(new Intent(AddPostActivity.this, PostListActivity.class));
+                                finish();
+
+
+
+                            }
+
+
+                        });
+
+
+                    }
+                }
+            });
+        }
+    }
+}
+
+
+
